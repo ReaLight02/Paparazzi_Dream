66b84afc709359f91841f4df5875bc86012c1162
ScherzoNo <105374360+ScherzoNo@users.noreply.github.com>
2024-06-20 18:17:54 +0200

    update

--- data_loader.py
+++ data_loader.py
@@ -2,21 +2,23 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Definisci i trasformatori di pre-elaborazione per le immagini di addestramento
 # Aggiungi trasformazioni per ruotare, zoomare e capovolgere orizzontalmente le immagini
-TrainingImagePath=TrainingImagePath = 'C:/Users/mocan/Desktop/Paparazzi_Dream/cropped/'
-train_datagen = ImageDataGenerator(
+TrainingImagePath=TrainingImagePath = 'E:/LAB/cropped/'
+train_datagen = ImageDataGenerator( 
     shear_range=0.1,
     zoom_range=0.1,
-    horizontal_flip=True
+    horizontal_flip=True,
+    rescale=1./255,
 )
 
-# Definisci il trasformatore di pre-elaborazione per le immagini di test (senza data augmentation)
-test_datagen = ImageDataGenerator()
+# Definisci il trasformatore di pre-elaborazione per le immagini di test
+test_datagen = ImageDataGenerator(validation_split=0.2, rescale=1./255,)
 
 # Genera dati di addestramento
 training_set = train_datagen.flow_from_directory(
     TrainingImagePath,
     target_size=(64, 64),
-    batch_size=32,
+    batch_size=64,
+    subset = 'training',
     class_mode='categorical'
 )
 
@@ -24,6 +26,7 @@ training_set = train_datagen.flow_from_directory(
 test_set = test_datagen.flow_from_directory(
     TrainingImagePath,
     target_size=(64, 64),
-    batch_size=32,
+    batch_size=64,
+    subset = 'validation',
     class_mode='categorical'
 )
