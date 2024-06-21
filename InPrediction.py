1c6dd6b3e42b87880b6e1d190df22bc70797ac61
ScherzoNo <105374360+ScherzoNo@users.noreply.github.com>
2024-06-21 20:24:22 +0200

    update

--- InPrediction.py
+++ InPrediction.py
@@ -1,13 +1,16 @@
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
+from cropping import get_cropped_image_2_eyes
+import cv2
 
 # Load the trained model
 model = load_model('fizzio.h5')
 
-# Preprocess the input image
-def preprocess_image(image_path):
-    img = image.load_img(image_path, target_size=(64, 64))  # Use the same target size as during training
+def preprocess_image(img_array):
+    # Assuming img_array is already in the correct format (e.g., uint8 or float32)
+    img = image.array_to_img(img_array, data_format=None, scale=True)
+    img = img.resize((64, 64))  # Resize to the target size (64x64)
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
     img_array /= 255.0  # Scale pixel values to [0, 1] range if rescale was used during training
@@ -28,10 +31,14 @@ def interpret_prediction(prediction):
     return predicted_class_name
 
 # Path to the input image
-image_path = 'E:\LAB\cropped\Hugh Jackman/Hugh Jackman9.jpg'
+image_path = 'E:\LAB/smith.jpg'
+
+cropped = get_cropped_image_2_eyes(image_path)
+cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB if needed
 
 # Classify the image and interpret the prediction
-prediction = classify_image(model, image_path)
-predicted_class_name = interpret_prediction(prediction)
+prediction = classify_image(model, cropped)
 
+predicted_class_name = interpret_prediction(prediction)
+    
 print(f"The predicted class for the input image is: {predicted_class_name}")
