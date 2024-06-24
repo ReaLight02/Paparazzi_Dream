77fa891f03ba1a35768936d67762701b92218ce1
ScherzoNo <105374360+ScherzoNo@users.noreply.github.com>
2024-06-24 12:35:10 +0200

    update

--- CNN/data_loader.py
+++ CNN/data_loader.py
@@ -9,14 +9,14 @@ train_datagen = ImageDataGenerator(
     zoom_range=0.2,
     horizontal_flip=True,
     rescale=1.0 / 255,
-    validation_split=0.2,
+    validation_split=0.1,
 )
 
 # Create training data
 training_set = train_datagen.flow_from_directory(
     TrainingImagePath,
     target_size=(64, 64),
-    batch_size=32,
+    batch_size=8,
     subset="training",
     class_mode="categorical",
     color_mode="rgb",
@@ -26,7 +26,7 @@ training_set = train_datagen.flow_from_directory(
 test_set = train_datagen.flow_from_directory(
     TrainingImagePath,
     target_size=(64, 64),
-    batch_size=32,
+    batch_size=8,
     subset="validation",
     class_mode="categorical",
     color_mode="rgb",
