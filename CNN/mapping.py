8e7aa6847f39270da507a982cff024cb296f10f8
ScherzoNo <105374360+ScherzoNo@users.noreply.github.com>
2024-06-24 11:28:51 +0200

    update

--- CNN/mapping.py
+++ CNN/mapping.py
@@ -1,23 +1,13 @@
-
 import data_loader
-'''############ Creating lookup table for all faces ############'''
-# class_indices have the numeric tag for each face
-TrainClasses=data_loader.training_set.class_indices
- 
-# Storing the face and the numeric tag for future reference
-ResultMap={}
-for faceValue,faceName in zip(TrainClasses.values(),TrainClasses.keys()):
-    ResultMap[faceValue]=faceName
- 
-# Saving the face map for future reference
-import pickle
-with open("ResultsMap.pkl", 'wb') as fileWriteStream:
-    pickle.dump(ResultMap, fileWriteStream)
- 
-# The model will give answer as a numeric tag
-# This mapping will help to get the corresponding face name for it
-print("Mapping of Face and its ID",ResultMap)
- 
-# The number of neurons for the output layer is equal to the number of faces
-OutputNeurons=len(ResultMap)
-print('\n The Number of output neurons: ', OutputNeurons)
+
+TrainClasses = data_loader.training_set.class_indices
+
+# Mapping face : id in ResultMap
+ResultMap = {}
+for faceValue, faceName in zip(TrainClasses.values(), TrainClasses.keys()):
+    ResultMap[faceValue] = faceName
+
+print("Mapping Face : Id", ResultMap)
+
+OutputNeurons = len(ResultMap)
+print("\n Output neurons: ", OutputNeurons)
