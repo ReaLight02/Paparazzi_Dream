10d12e45cca1908043a26162c2aca19544ce4060
ScherzoNo <105374360+ScherzoNo@users.noreply.github.com>
2024-06-24 11:28:10 +0200

    update

--- CNN/cropping.py
+++ CNN/cropping.py
@@ -1,21 +1,19 @@
-import numpy as np
-import cv2
 import os
+import cv2
 import shutil
-import matplotlib
-from matplotlib import pyplot as plt
+
 
 def get_cropped_image_2_eyes(img_path):
-    
-    face_detector = cv2.CascadeClassifier('E:\LAB\haarcascade_frontalface_default.xml')
+
+    face_detector = cv2.CascadeClassifier("E:\LAB\haarcascade_frontalface_default.xml")
     eye_detector = cv2.CascadeClassifier("E:\LAB\haarcascade_eye.xml")
-    
+
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray,1.3,5)
-    for(x,y,w,h) in faces:
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    for x, y, w, h in faces:
+        roi_gray = gray[y : y + h, x : x + w]
+        roi_color = img[y : y + h, x : x + w]
         eyes = eye_detector.detectMultiScale(roi_gray)
         if len(eyes) >= 2:
             return roi_color
@@ -23,11 +21,10 @@ def get_cropped_image_2_eyes(img_path):
 
 if __name__ == "__main__":
 
-    path_to_data='E:\LAB\Celebrity Faces Dataset'
-    percorso_directory ='E:/LAB/cropped/'
-    os.makedirs(percorso_directory, exist_ok=True)
-    path_to_cropped='E:/LAB/cropped/'
-    img_dirs =[]
+    path_to_data = "E:\LAB\Celebrity Faces Dataset"
+    path_to_cropped = "E:/LAB/cropped/"
+    os.makedirs(path_to_cropped, exist_ok=True)
+    img_dirs = []
     for entry in os.scandir(path_to_data):
         if entry.is_dir():
             img_dirs.append(entry.path)
@@ -35,16 +32,15 @@ if __name__ == "__main__":
         shutil.rmtree(path_to_cropped)
     os.mkdir(path_to_cropped)
 
-    cropped_img_dirs =[]
-    celeb_file_names_dict ={}
+    cropped_img_dirs = []
+    celeb_file_names_dict = {}
 
     for img_dir in img_dirs:
         count = 1
-        celeb_name = img_dir.split('\\')[-1]
-
-        celeb_file_names_dict[celeb_name]=[]
-
+        celeb_name = img_dir.split("\\")[-1]
+        celeb_file_names_dict[celeb_name] = []
         print(celeb_name)
+
         for entry in os.scandir(img_dir):
             roi_color = get_cropped_image_2_eyes(entry.path)
             if roi_color is not None:
@@ -52,11 +48,11 @@ if __name__ == "__main__":
                 if not os.path.exists(cropped_folder):
                     os.makedirs(cropped_folder)
                     cropped_img_dirs.append(cropped_folder)
-                    print("generating in folder", cropped_folder)
-                cropped_file_name = celeb_name + str(count) +".jpg"
-                cropped_file_path = cropped_folder +"\\" + cropped_file_name
-            
-                cv2.imwrite(cropped_file_path,roi_color)
+                    print("Generating", cropped_folder)
+                cropped_file_name = celeb_name + str(count) + ".jpg"
+                cropped_file_path = cropped_folder + "\\" + cropped_file_name
+
+                cv2.imwrite(cropped_file_path, roi_color)
 
                 celeb_file_names_dict[celeb_name].append(cropped_file_path)
-                count +=1
+                count += 1
